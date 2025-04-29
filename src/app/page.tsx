26fd9fec0631fcacd6feb6c5b6950bb78fbae49c
Akhil <akhilarun187@gmail.com>
2025-04-29 10:45:32 +0800

    docs(page.tsx): add metadata for SEO and social sharing
    Added metadata configuration to the home page to improve SEO and enhance social sharing capabilities. The metadata includes descriptions for search engines, OpenGraph, and Twitter cards, ensuring consistent and optimized content presentation across platforms.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,25 @@ import {
   getAllCategoriesSortedByPostCount,
 } from "@/lib/posts"; // Import getPopularPostsData
 import BlogClient from "@/components/BlogClient";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  description:
+    "Explore the latest in autonomous technology on AutoTek. Read featured articles, discover trending topics, browse popular posts, and dive into categories covering self-driving cars, AI, robotics, and more.",
+
+  alternates: {
+    canonical: "/",
+  },
+
+  openGraph: {
+    description:
+      "Explore the latest in autonomous technology on AutoTek. Read featured articles, discover trending topics, browse popular posts, and more.",
+  },
+  twitter: {
+    description:
+      "Explore the latest in autonomous technology on AutoTek. Read featured articles, discover trending topics, browse popular posts, and more.",
+  },
+};
 
 export default async function Home() {
   const posts = await getSortedPostsData();
